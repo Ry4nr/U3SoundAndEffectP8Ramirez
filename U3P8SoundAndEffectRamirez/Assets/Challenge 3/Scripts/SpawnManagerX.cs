f4fd0cd22dbd45988d006e4a72e93d8da51d53fa
Ry4nr <756238@student.dusd.net>
2025-05-12 14:38:23 -0700

    Done
    finished my challenge 3

--- U3P8SoundAndEffectRamirez/Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ U3P8SoundAndEffectRamirez/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] objectPrefabs;
     private float spawnDelay = 2;
     private float spawnInterval = 1.5f;
-
     private PlayerControllerX playerControllerScript;
-
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
-
     // Spawn obstacles
-    void SpawnObjects ()
+    void SpawnObjects()
     {
         // Set random spawn location and random object index
         Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
@@ -29,6 +30,6 @@ public class SpawnManagerX : MonoBehaviour
         {
             Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
         }
-
     }
 }
+
